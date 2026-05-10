ccf55efdb22d6c7cc165553e32a73cd82c33adb0
janko444 <ncall5502@gmail.com>
2026-05-10 10:40:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const humidity = document.getElementById("humidity");
 const wind = document.getElementById("wind");
 const weatherIcon = document.getElementById("weatherIcon");
 
-// MAPA
+// MAPA (Leaflet)
 const map = L.map('map').setView([52.2297, 21.0122], 6);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -17,22 +17,45 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 let marker;
 
-// klik na mapie
+// 📍 klik na mapie
 map.on("click", async function(e){
 
   const lat = e.latlng.lat;
   const lon = e.latlng.lng;
 
-  if(marker){
-    map.removeLayer(marker);
-  }
-
-  marker = L.marker([lat, lon]).addTo(map);
-
+  setMarker(lat, lon);
   getWeatherByCoords(lat, lon);
 
 });
 
+// 📌 marker
+function setMarker(lat, lon){
+  if(marker){
+    map.removeLayer(marker);
+  }
+  marker = L.marker([lat, lon]).addTo(map);
+}
+
+// 🌍 reverse geocoding (ładna nazwa miejsca)
+async function getCityName(lat, lon){
+
+  try{
+    const url = `https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lon}&format=json`;
+    const res = await fetch(url);
+    const data = await res.json();
+
+    return data.address.city ||
+           data.address.town ||
+           data.address.village ||
+           data.address.state ||
+           data.display_name;
+
+  } catch(err){
+    return "Nieznana lokalizacja";
+  }
+}
+
+// 🌦️ pogoda po współrzędnych
 async function getWeatherByCoords(lat, lon){
 
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
@@ -40,13 +63,21 @@ async function getWeatherByCoords(lat, lon){
   const res = await fetch(url);
   const data = await res.json();
 
-  city.innerText = data.name || "Nieznane miejsce";
+  const placeName = await getCityName(lat, lon);
+
+  city.innerText = placeName || "Nieznana lokalizacja";
   temperature.innerText = Math.round(data.main.temp) + "°C";
   description.innerText = data.weather[0].description;
   humidity.innerText = data.main.humidity + "%";
   wind.innerText = data.wind.speed + " km/h";
 
   const type = data.weather[0].main;
+
+  changeTheme(type);
+}
+
+// 🎨 zmiana wyglądu pogody
+function changeTheme(type){
 
   if(type === "Clouds"){
     weatherIcon.src = "https://cdn-icons-png.flaticon.com/512/414/414825.png";
@@ -72,3 +103,28 @@ async function getWeatherByCoords(lat, lon){
     weatherIcon.src = "https://cdn-icons-png.flaticon.com/512/1163/1163661.png";
   }
 }
+
+// 📍 MOJA LOKALIZACJA (NOWOŚĆ)
+function myLocation(){
+
+  if(navigator.geolocation){
+
+    navigator.geolocation.getCurrentPosition((pos)=>{
+
+      const lat = pos.coords.latitude;
+      const lon = pos.coords.longitude;
+
+      map.setView([lat, lon], 10);
+
+      setMarker(lat, lon);
+      getWeatherByCoords(lat, lon);
+
+    });
+
+  } else {
+    alert("Geolokalizacja nie jest wspierana");
+  }
+}
+
+// 🟢 start aplikacji
+getWeatherByCoords(52.2297, 21.0122);
